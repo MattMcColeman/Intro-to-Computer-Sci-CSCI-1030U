fe408e89a7ab572055a2533e0eaf9298b391b372
arbitrator8 <matt.mccoleman@gmail.com>
2016-01-24 22:22:59 -0500

    restored original file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,5 @@
 public class HelloWorld{
 	public statuc void main(string[] args){
-<<<<<<< HEAD
-<<<<<<< HEAD
-		System.out.printf("Hello Canada idk/n");
-=======
-		System.out.printf("Hello Canada Quantum2/n");
->>>>>>> quantum2
-=======
-		System.out.printf("Hello Canada idk/n");
->>>>>>> quantum2
+		System.out.printf("Hello World/n");
 	}
 }
